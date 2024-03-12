022be4c7c2cdaf6cec6fd55d1a702c5e305bc416
kristinnornk <58362287+kristinnornk@users.noreply.github.com>
2024-03-12 09:34:31 +0000

    Update add.ts

--- src/utilities/notifications/add.ts
+++ src/utilities/notifications/add.ts
@@ -2,7 +2,9 @@ import { collection, addDoc } from "firebase/firestore";
 import { db } from "../../firebase/init";
 import { TNotificationRead } from "../../types/baseTypes";
 
-export async function addNotification(notification: TNotificationRead) {
+export async function addNotification(
+  notification: Omit<TNotificationRead, "nid">
+) {
   const notificationsRef = collection(db, "notifications");
 
   return await addDoc(notificationsRef, notification)
