d89bc5601d5eaabb6b249812bf9b5bb5c21e0b84
Elías Nökkvi <93617767+elias-nokkvi@users.noreply.github.com>
2024-07-12 18:21:06 +0000

    fixing fetching companies for admin

--- src/utilities/admin/companies/get.ts
+++ src/utilities/admin/companies/get.ts
@@ -1,29 +1,32 @@
-import { collection, getDocs } from "firebase/firestore";
-import { companyConverter } from "../../../converters/company";
-import { db } from "../../../firebase/init";
-import { TCompanyWithCreator } from "../../../types/companyTypes";
-import { getEmployer } from "../../users/get";
+import { collection, getDocs } from 'firebase/firestore';
+import { companyConverter } from '../../../converters/company';
+import { db } from '../../../firebase/init';
+import { TCompanyWithCreator } from '../../../types/companyTypes';
+import { getEmployer } from '../../users/get';
 
 export async function getCompanies(): Promise<TCompanyWithCreator[]> {
-  const companiesRef = collection(db, "companies").withConverter(
+  const companiesCollectionRef = collection(db, 'companies').withConverter(
     companyConverter
   );
-  const companiesSnap = await getDocs(companiesRef);
-  const employeesCollection = collection(companiesRef, "employees");
-  const employeesSnap = await getDocs(employeesCollection);
+  const companiesSnap = await getDocs(companiesCollectionRef);
 
-  const companiesPromise = companiesSnap.docs.map(async (doc) => {
+  const companiesPromise = companiesSnap.docs.map(async doc => {
     try {
       const company = doc.data();
-      const creator = await getEmployer(company.creator.id);
+      const employeesCollection = collection(doc.ref, 'employees');
+      const [creator, employeesSnap] = await Promise.all([
+        getEmployer(company.creator.id),
+        getDocs(employeesCollection),
+      ]);
+
       return {
         ...company,
         creator,
-        employeesCount: employeesSnap?.docs.length || 0,
+        employeesCount: employeesSnap.docs.length || 0,
       };
     } catch (error) {
-      console.log("Error getting company:", doc.id);
-      throw new Error("Errror getting company!");
+      console.log('Error getting company:', doc.id);
+      throw new Error('Errror getting company!');
     }
   });
 
