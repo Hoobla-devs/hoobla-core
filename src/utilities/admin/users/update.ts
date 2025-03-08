768e59936e5a0f1455582e59f015faeca4e4bf4a
Elías Nökkvi <93617767+elias-nokkvi@users.noreply.github.com>
2025-03-08 14:19:58 +0000

    prettier formatting

--- src/utilities/admin/users/update.ts
+++ src/utilities/admin/users/update.ts
@@ -4,68 +4,55 @@ import {
   doc,
   DocumentReference,
   Timestamp,
-} from "firebase/firestore";
-import { db } from "../../../firebase/init";
+} from 'firebase/firestore';
+import { db } from '../../../firebase/init';
 import {
   TFreelancerStatus,
   TFreelancerUnapprovedTags,
   TFreelancerUser,
   TGeneral,
   TUserWrite,
-} from "../../../types/userTypes";
-import { updateDoc } from "../../updateDoc";
+} from '../../../types/userTypes';
+import { updateDoc } from '../../updateDoc';
 
-/**
- * Update status of freelancer application.
- * Base usage is when approving or denying user from becoming a verified freelancer
- * @param uid
- * @param status
- * @returns
- */
 export async function updateFreelancerStatus(
   uid: string,
   status: TFreelancerStatus
 ) {
-  const freelancerRef = doc(db, "users", uid) as DocumentReference<TUserWrite>;
+  const freelancerRef = doc(db, 'users', uid) as DocumentReference<TUserWrite>;
 
   return await updateDoc(freelancerRef, {
-    "freelancer.status": status,
-    "general.updatedAt": Timestamp.now(),
+    'freelancer.status': status,
+    'general.updatedAt': Timestamp.now(),
     // if status is inactive, add inactiveSince date. Else, remove field
-    "freelancer.inactiveSince":
-      status === "inactive" ? Timestamp.now() : deleteField(),
+    'freelancer.inactiveSince':
+      status === 'inactive' ? Timestamp.now() : deleteField(),
   })
     .then(() => true)
-    .catch((err) => false);
+    .catch(err => false);
 }
 
-/**
- * Add a new contract id to freelancer to sign.
- * @param freelancerUser
- * @param documentId id of Signet Api document
- * @returns
- */
 export async function addFreelancerContract(
   freelancerUser: TFreelancerUser,
   documentId: string
 ) {
   const userRef = doc(
     db,
-    "users",
+    'users',
     freelancerUser.general.uid
   ) as DocumentReference<TUserWrite>;
 
   const newStatus =
-    freelancerUser.freelancer.status === "inReview"
-      ? "requiresSignature"
+    freelancerUser.freelancer.status === 'inReview'
+      ? 'requiresSignature'
       : freelancerUser.freelancer.status;
 
   return await updateDoc(userRef, {
-    "freelancer.contract": {
+    'freelancer.contract': {
       documentId,
       signed: false,
     },
-    "freelancer.status": newStatus,
+    'freelancer.status': newStatus,
   })
     .then(() => {
       return true;
@@ -76,15 +63,15 @@ export async function addFreelancerContract(
 export async function addTagToFreelancer(
   uid: string,
   tagId: string,
-  type: "jobTitles" | "skills" | "languages",
+  type: 'jobTitles' | 'skills' | 'languages',
   updatedUnapprovedTags: TFreelancerUnapprovedTags | null
 ) {
-  const userRef = doc(db, "users", uid) as DocumentReference<TUserWrite>;
+  const userRef = doc(db, 'users', uid) as DocumentReference<TUserWrite>;
   const tagField = `freelancer.${type}`;
 
   return await updateDoc(userRef, {
     [tagField]: arrayUnion(tagId),
-    "freelancer.unapprovedTags": updatedUnapprovedTags,
+    'freelancer.unapprovedTags': updatedUnapprovedTags,
   })
     .then(() => true)
     .catch(() => false);
@@ -95,7 +82,7 @@ export async function updateGeneralField(
   field: keyof TGeneral,
   value: TGeneral[keyof TGeneral]
 ) {
-  const userRef = doc(db, "users", uid) as DocumentReference<TUserWrite>;
+  const userRef = doc(db, 'users', uid) as DocumentReference<TUserWrite>;
 
   return await updateDoc(userRef, {
     [`general.${field}`]: value,
