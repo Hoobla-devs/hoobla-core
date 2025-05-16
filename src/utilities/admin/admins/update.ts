959087136e72ce328718f0892e900322745f2695
Elías Nökkvi <93617767+elias-nokkvi@users.noreply.github.com>
2025-05-16 12:07:26 +0000

    temporarily removing admin invite functionality

--- src/utilities/admin/admins/update.ts
+++ src/utilities/admin/admins/update.ts
@@ -12,23 +12,20 @@ const token = function () {
 };
 
 export async function addAdminInvites(user: TAdmin, emails: string[]) {
-  const invites: TInvite[] = emails.map((email, i) => ({
-    name: '', // TODO: Implement this
-    position: '',
-    role: 'admin',
-    email: email,
-    token: token(),
-    date: new Date(),
-  }));
-
-  const adminRef = doc(db, 'admins', user.uid);
-  const success = await updateDoc(adminRef, {
-    invites: [...user.invites, ...invites],
-  })
-    .then(() => true)
-    .catch(err => false);
-
-  if (success) return { success: true, invites };
-
-  return { success: false, invites: [] };
+  // const invites: TInvite[] = emails.map((email, i) => ({
+  //   name: '', // TODO: Implement this
+  //   position: '',
+  //   role: 'admin',
+  //   email: email,
+  //   token: token(),
+  //   date: new Date(),
+  // }));
+  // const adminRef = doc(db, 'admins', user.uid);
+  // const success = await updateDoc(adminRef, {
+  //   invites: [...user.invites, ...invites],
+  // })
+  //   .then(() => true)
+  //   .catch(err => false);
+  // if (success) return { success: true, invites };
+  // return { success: false, invites: [] };
 }
